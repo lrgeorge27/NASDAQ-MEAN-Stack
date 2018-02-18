38f2ce3ca7777cc950cee62f00d50e2b3938b666
lrgeorge27 <lrgeorge27@gmail.com>
2018-02-18 01:38:57 +0000

    added dependencies, app.js, mongoose model shell

---
+++ app.js
@@ -0,0 +1,34 @@
+//NASDAQ APP - Main node file
+var express = require('express');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var routes = require('./api/routes');
+var path = require('path');
+
+var app = express();
+
+app.set('port', process.env.PORT);
+
+app.use(express.static(path.join(__dirname, 'public')));
+// app.use('/node-modules', express.static(__dirname + '/node_modules'));
+
+//add middleware and enable parsing
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+//add routing 
+app.use('/api', routes); //looks in /api/routes folder with only / beginning path
+
+//connect to mongoDB
+mongoose.connect('mongodb://lrgeorge27-firstworkspace-5563394/nasdaq');
+mongoose.connection.once('open', function(){
+    console.log("Mongoose is listening");
+});
+
+var server = app.listen(app.get('port'), function(){
+    var port = server.address().port;
+    console.log("Listening on port " + port);
+});
+    
+
+
---
+++ models/stocksModel.js
@@ -0,0 +1,11 @@
+//NASDAQ mongoose model
+var mongoose = require('mongoose');
+
+//create Schema
+var stockSchema = new mongoose.Schema({
+    //add keys here
+});
+
+
+//export model schema
+module.exports = stockSchema;
